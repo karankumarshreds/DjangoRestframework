1ef79a3dc31f7720dde90bc56cbc1ca404c11bb8
karankumarshreds <naileditnerd@gmail.com>
2021-02-10 04:01:21 +0530

    add admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Student
 
-# Register your models here.
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+  list_display = ['id', 'name', 'roll']
+
+## OR simply 
+
+# admin.site.register(Search)
+# admin.site.register(Profile)
+# admin.site.register(Post)
+# admin.site.register(Likes)
+
---
+++ startup/readme.md
@@ -0,0 +1,19 @@
+# Startup commands
+
+## Create a project
+
+`django-admin startproject <project-name>`
+
+## Create an app
+
+`python manage.py startapp <app-name>`
+
+## Migrations
+
+`python manage.py makemigrations`
+
+`python manage.py migrate`
+
+## Admin
+
+`python manage.py createsuperuser`
