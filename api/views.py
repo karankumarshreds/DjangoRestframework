3eccb07f89c39e0695ed5fb48f5d63bc638f8e03
karankumarshreds <naileditnerd@gmail.com>
2021-02-10 12:41:26 +0530

    add first view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.renderers import JSONRenderer
+from models import Student
+from serializers import StudentSerializer as SS
 
-# Create your views here.
+# single model object 
+def student_detail(req):
+  stu = Student.objects.get(id = 1)
+  serializer = SS(stu)
+  jsonData = JSONRenderer().render(serializer.data)
+  return HttpResponse(jsonData, content_type='application/json')
